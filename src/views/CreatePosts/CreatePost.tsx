3bcbb3ea8ea64a7d57e1986b6a620c12501a7735
Wyatt Haston <awhaston@protonmail.com>
2023-03-02 15:02:30 -0500

    Added uuid

--- src/views/CreatePosts/CreatePost.tsx
+++ src/views/CreatePosts/CreatePost.tsx
@@ -1,9 +1,18 @@
 import MDEditor from '@uiw/react-md-editor';
-import { addDoc, collection, Timestamp } from 'firebase/firestore';
+import {
+	addDoc,
+	collection,
+	doc,
+	Timestamp,
+	updateDoc,
+} from 'firebase/firestore';
 import { getDownloadURL, ref, uploadBytesResumable } from 'firebase/storage';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import { v4 as uuidv4 } from 'uuid';
 import { usePosts } from '../../contexts/PostContext';
 import { db, storage } from '../../firebase';
+import { ResultPost } from '../../types';
 import './CreatePost.css';
 
 function CreatePost() {
@@ -14,15 +23,54 @@ function CreatePost() {
 	const [percent, setPercent] = useState(0);
 	const [tags, setTags] = useState<string>('');
 	const [loading, setLoading] = useState(false);
+	let isUpdating = false;
+	let { id }: any = useParams();
+	id = parseInt(id);
+	const { data } = usePosts();
+
+	useEffect(() => {
+		if (id && data[0]?.title) {
+			const post = data.find((post) => post.id === id);
+			setTitle(post!.title);
+			setBody(post!.body);
+			setTags(post!.tags.toString());
+			isUpdating = true;
+		}
+		console.log(isUpdating);
+		console.log(data);
+	}, [data, id]);
 
 	//Uploads header image before calling post
 	const handleSubmit = (e: any) => {
 		setLoading(true);
 		e.preventDefault();
-		if (!file) {
+		// if (isUpdating) {
+		// 	return updatePost(id);
+		// }
+		if (!file && !isUpdating) {
 			setLoading(false);
 			return setMessage('Please Enter a header image!');
+		} else {
+			uploadImage();
 		}
+	};
+
+	// async function updatePost(id: number) {
+	// 	const docRef = doc(db, 'posts', 'id');
+	// 	const tagArray = tags.replace(/\s/g, '').split(',');
+
+	// 	const updatedPost = {
+	// 		body: body,
+	// 		tags: tagArray,
+	// 		title: title,
+	// 	};
+
+	// 	await updateDoc(docRef, updatedPost).then((res) => {
+	// 		console.log(res);
+	// 	});
+	// }
+
+	function uploadImage() {
 		const storageRef = ref(storage, `/images/${file.name}`);
 		const uploadTask = uploadBytesResumable(storageRef, file);
 		uploadTask.on(
@@ -36,21 +84,20 @@ function CreatePost() {
 			(err) => console.log(err),
 			() => {
 				getDownloadURL(uploadTask.snapshot.ref).then((url) => {
-					console.log(url);
 					createPost(url);
 				});
 			}
 		);
-	};
+	}
 
 	// Uploading post to db after getting image url
-	const createPost = (url: string) => {
-		const { data }: any = usePosts();
-		const id = data.length - 1;
+	const createPost = async (url: string) => {
+		const uid = uuidv4();
 		try {
 			const tagArray = tags.replace(/\s/g, '').split(',');
-			addDoc(collection(db, 'posts'), {
-				id: id,
+			const docRef: any = doc(db, 'posts');
+			await addDoc(docRef, {
+				id: uid,
 				title: title,
 				createdAt: Timestamp.now(),
 				tags: tagArray,
@@ -66,6 +113,7 @@ function CreatePost() {
 		setMessage('Post completed');
 	};
 
+	//TODO: Type Events
 	const handleImage = (e: any) => {
 		setFile(e.target.files[0]);
 	};
@@ -86,6 +134,9 @@ function CreatePost() {
 					type='text'
 					value={title}
 					onChange={(e: any) => {
+						if (!e.target.value) {
+							return;
+						}
 						setTitle(e.target.value);
 					}}
 				/>
