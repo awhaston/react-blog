0055d7eb5afdeb14523654b561c2dbafc0618d4c
Wyatt Haston <awhaston@protonmail.com>
2023-03-05 22:33:32 -0500

    Updated posts on homepage

--- src/views/Home/Home.tsx
+++ src/views/Home/Home.tsx
@@ -42,48 +42,41 @@ function Home() {
 	});
 
 	useEffect(() => {
-		if (data[0]?.title) {
-			setMainPost({
-				...mainPost,
-				title: data[0]?.title,
-				header_url: data[0]?.header_url,
-				body: data[0]?.body.slice(0, 400) + '...',
-				createdAt: data[0]?.createdAt,
-				id: data[0]?.id,
-			});
+		if (!data[0]?.title) return;
 
-			setPostOne({
-				...postOne,
-				title:
-					data[0 + 1]?.title.length > 100
-						? data[0 + 1]?.title.slice(0, 100) + '...'
-						: data[0 + 1]?.title,
-				header_url: data[0 + 1]?.header_url,
-				createdAt: data[0 + 1]?.createdAt,
-				id: data[0 + 1]?.id,
-			});
-			setPostTwo({
-				...postTwo,
-				title:
-					data[0 + 2]?.title.length > 100
-						? data[0 + 2]?.title.slice(0, 100) + '...'
-						: data[0 + 2]?.title,
-				header_url: data[0 + 2]?.header_url,
-				createdAt: data[0 + 2]?.createdAt,
-				id: data[0 + 2]?.id,
-			});
-			setPostThree({
-				...postThree,
-				title:
-					data[0 + 3]?.title.length > 100
-						? data[0 + 3]?.title.slice(0, 100) + '...'
-						: data[0 + 3]?.title,
-				header_url: data[0 + 3]?.header_url,
-				createdAt: data[0 + 3]?.createdAt,
-				id: data[0 + 3]?.id,
-			});
-			console.log(data);
-		}
+		setMainPost({
+			...mainPost,
+			title: data[0]?.title,
+			header_url: data[0]?.header_url,
+			body: data[0]?.body.slice(0, 400) + '...',
+			createdAt: data[0]?.createdAt,
+			id: data[0]?.id,
+		});
+
+		setPostOne({
+			...postOne,
+			title: data[0 + 1]?.title,
+			header_url: data[0 + 1]?.header_url,
+			body: data[0 + 1]?.body,
+			createdAt: data[0 + 1]?.createdAt,
+			id: data[0 + 1]?.id,
+		});
+		setPostTwo({
+			...postTwo,
+			title: data[0 + 2]?.title,
+			body: data[0 + 2]?.body,
+			header_url: data[0 + 2]?.header_url,
+			createdAt: data[0 + 2]?.createdAt,
+			id: data[0 + 2]?.id,
+		});
+		setPostThree({
+			...postThree,
+			title: data[0 + 3]?.title,
+			body: data[0 + 3]?.body,
+			header_url: data[0 + 3]?.header_url,
+			createdAt: data[0 + 3]?.createdAt,
+			id: data[0 + 3]?.id,
+		});
 	}, [data]);
 
 	return (
@@ -106,9 +99,9 @@ function Home() {
 			<h2 className='home-post-header'>More Posts...</h2>
 
 			<div className='home-post-container'>
-				<Post props={postOne} classes='home-post' />
-				<Post props={postTwo} classes='home-post' />
-				<Post props={postThree} classes='home-post' />
+				<Post props={postOne} classes='' />
+				<Post props={postTwo} classes='' />
+				<Post props={postThree} classes='' />
 			</div>
 		</div>
 	);
